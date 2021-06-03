59b2bcb7ceececa260ba88d77a88b9b5be6405ef
Marrwan <Abdulbasitdamilola6@gmail.com>
2021-06-03 16:14:13 +0100

    changed

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-const logger = require("morgan");
 const expressLayout = require("express-layouts");
 const mongoose = require("mongoose");
 const session = require("express-session");
@@ -30,7 +29,6 @@ app.set("view engine", "ejs");
 
 app.use(methodOverride("_method"));
 app.use(expressLayout);
-// app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
